217309d12c69fb0ad1b1464dd5d1a4947a8fc807
Daniele <danieledetommaso1995@gmail.com>
2020-06-10 17:33:43 +0200

    Consegna esercizio

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(
+  function(){
+
+    $('li').click(
+      function(){
+        $(this).children('.dropdown').toggleClass('active');
+      }
+    );
+
+    $('li').mouseover(
+      function(){
+        $(this).children('.freccia').html('&#9652;');
+        $(this).children('a').addClass('color-black');
+        $(this).children('.dropdown').addClass('active');
+      }
+    );
+
+    $('li').mouseout(
+      function(){
+        $(this).children('.freccia').html('&#9662;');
+        $(this).children('a').removeClass('color-black');
+        $(this).children('.dropdown').removeClass('active');
+      }
+    );
+
+
+
+
+  }
+);
