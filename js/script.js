dc7752c5bb35fa413aad543d3e1a4e61cc23dd89
Daniele <danieledetommaso1995@gmail.com>
2020-06-11 16:12:33 +0200

    Sistemazioni varie

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(
   function(){
 
+    // Al click del link fa apparire il sottomenú
     $('li').click(
       function(){
         $(this).children('.dropdown').toggleClass('active');
       }
     );
 
+    // Passando sopra al link con il mouse fa apparire il sottomenú e i vari stili
     $('li').mouseover(
       function(){
         $(this).children('.freccia').html('&#9652;');
@@ -15,6 +17,7 @@ $(document).ready(
       }
     );
 
+    // Allontanandosi dal link con il mouse fa sparire il sottomenú e i vari stili
     $('li').mouseout(
       function(){
         $(this).children('.freccia').html('&#9662;');
@@ -23,20 +26,19 @@ $(document).ready(
       }
     );
 
+    // Passando sopra al link del sottomenu con il mouse fa apparire i vari stili
     $('.item').mouseover(
       function(){
         $(this).children('a').addClass('background-gray');
       }
     );
 
+    // Allontanandosi da sopra ai link del sottomenu con il mouse fa apparire i vari stili
     $('.item').mouseout(
       function(){
         $(this).children('a').removeClass('background-gray');
       }
     );
-
-
-
 
   }
 );
